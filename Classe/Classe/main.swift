3741d1a30499c03ca6a394fc846ce0deae34c517
Yuri <yuriyra@gmail.com>
2023-03-07 18:29:33 -0500

    Codigo Caio

---
+++ Classe/Classe/main.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  Classe
+//
+//  Created by Yuri Alencar on 2023-03-07.
+//
+
+import Foundation
+
+// OBJETO
+// CARACTERISTICAS DE UM OBJETO -> ATRIBUTOS OU PROPRIEDADES
+// AÇÕES = FUNÇÃO / METODO
+
+// Para criar uma classe sempre começamos com a palavra reservada "class" e em seguida colocamos o seu nome que sempre deve começar com a primeira letra maiusculo
+
+class Automovel {
+    
+    var cor: String = "Preto"
+    var numeroDePortas: Int = 2
+    var marca: String = "Honda"
+    var eEletrico: Bool = false
+    var quantidadeDePassageiros: Int = 5
+    
+    func ligarFarol() {
+        print("O Farol está ligado")
+    }
+    
+    func ligarOCarro() {
+        print("O carro está ligado")
+    }
+}
+
